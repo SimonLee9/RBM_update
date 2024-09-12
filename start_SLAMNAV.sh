d91791702d1fa4feb4e64eb3646a0c51069e99b3
SimonLee9 <lbj142632@gmail.com>
2024-09-12 11:18:28 +0900

    upate: for slamnav : orbeck

--- start_SLAMNAV.sh
+++ start_SLAMNAV.sh
@@ -4,7 +4,7 @@ sleep 1
 source ~/.bashrc
 
 
-#export LD_LIBRARY_PATH=/home/rainbow/OrbbecSDK/lib/linux_x64/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/rainbow/OrbbecSDK/lib/linux_x64/:$LD_LIBRARY_PATH
 #gnome-terminal -- bash -c "cd /home/rainbow/RBM-S100-release/ && ./SLAMNAV2; exec bash"
 
 gnome-terminal -- bash -c "cd /home/rainbow/code/build-SLAMNAV2-Desktop-Release/ && ./SLAMNAV2; exec bash"
