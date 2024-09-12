d9e87322dc47276eee6543e8164f08e14802036b
warhammer50K <pixvnt151@gmail.com>
2024-09-12 11:13:20 +0900

    update: for SLAM path

--- start_SLAMNAV.sh
+++ start_SLAMNAV.sh
@@ -2,8 +2,9 @@
 
 sleep 1
 source ~/.bashrc
-#find / -name "libOrbbecSDK.so.1.10"
-export LD_LIBRARY_PATH=/home/rainbow/OrbbecSDK/lib/linux_x64/:$LD_LIBRARY_PATH
 
 
-gnome-terminal -- bash -c "cd /home/rainbow/RBM-S100-release/ && ./SLAMNAV2; exec bash"
+#export LD_LIBRARY_PATH=/home/rainbow/OrbbecSDK/lib/linux_x64/:$LD_LIBRARY_PATH
+#gnome-terminal -- bash -c "cd /home/rainbow/RBM-S100-release/ && ./SLAMNAV2; exec bash"
+
+gnome-terminal -- bash -c "cd /home/rainbow/code/build-SLAMNAV2-Desktop-Release/ && ./SLAMNAV2; exec bash"
