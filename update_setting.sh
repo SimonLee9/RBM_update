2f106ec78a7972e75243aaf189f55f6e7d5c338d
SimonLee9 <lbj142632@gmail.com>
2024-11-28 10:32:26 +0900

    udpate

--- update_setting.sh
+++ update_setting.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-# 1번 (bashrc)
+# 1번 - bashrc
 # /etc/profile에 환경 변수 추가
 sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/usr/local/lib" >> /etc/profile'
 sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/rplidar_sdk/output/Linux/Release" >> /etc/profile'
@@ -13,7 +12,7 @@ source /etc/profile
 # 라이브러리 캐시 업데이트
 sudo ldconfig
 
-# 2번 (USB 절전 모드 해제)
+# 2번 (USB 전원 해제)
 # /etc/default/grub 파일 수정하여 usbcore.autosuspend=-1 추가
 sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ usbcore.autosuspend=-1 intel_pstate=disable"/' /etc/default/grub
 
@@ -24,16 +23,23 @@ sudo update-grub
 # sudo reboot
 
 # 무선 네트워크 전원 관리 비활성화
-sudo iwconfig wlo1 power off
+# 무선 인터페이스 이름을 실제 이름으로 변경하세요
+#sudo iwconfig <무선인터페이스이름> power off
 
-# 자동 업데이트 비활성화  (우분투 업데이트 설정 변경)
-
-# 자동 업데이트 확인 주기 설정: Never
-sudo sh -c 'echo "APT::Periodic::Update-Package-Lists \"0\";" > /etc/apt/apt.conf.d/10periodic'
+# 3번
+# 자동 업데이트 설정 비활성화
+sudo sh -c 'cat > /etc/apt/apt.conf.d/20auto-upgrades <<EOF
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF'
 
 # 새로운 우분투 버전 알림 설정: Never
-sudo sed -i 's/^Prompt=.*/Prompt=never/' /etc/update-manager/release-upgrades
+sudo sed -i 's/^Prompt=.*/Prompt=never/' /etc/update-manager/release-upgrades'
 
+# gsettings를 사용하여 자동 업데이트 확인 주기를 Never로 설정
+gsettings set com.ubuntu.update-notifier regular-auto-launch-interval 0
 
 echo "환경 설정이 완료되었습니다."
 
