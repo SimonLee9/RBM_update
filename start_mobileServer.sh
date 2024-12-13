006b86d824dbcff51deb66aa4c68a9700673f251
SimonLee9 <lbj142632@gmail.com>
2024-12-13 13:37:24 +0900

    send file update

--- start_mobileServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 1
-source ~/.bashrc
-gnome-terminal -- bash -c "cd /home/rainbow/MobileServer/ && node ./src/server.js; exec bash"
-
--- start_taskmanager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sleep 1
-
-gnome-terminal -- /bin/bash -c "cd /home/rainbow/TaskMan/build/; exec bash"
-gnome-terminal -- /bin/bash -c "./Map_Editer; exec bash"
