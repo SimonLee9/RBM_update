ebf02aff889b52060f913a8ffd7369e601fc2c93
SimonLee9 <lbj142632@gmail.com>
2024-12-18 14:47:07 +0900

    update: change directory

--- update-pc_setting.sh
+++ update-pc_setting.sh
@@ -4,7 +4,7 @@
 # /etc/profile에 환경 변수 추가
 sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/usr/local/lib" >> /etc/profile'
 sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/rplidar_sdk/output/Linux/Release" >> /etc/profile'
-sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/OrbbecSDK/lib/linux_x64" >> /etc/profile'
+#sudo sh -c 'echo "export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/OrbbecSDK/lib" >> /etc/profile'
 
 # 프로필 재적용
 source /etc/profile
