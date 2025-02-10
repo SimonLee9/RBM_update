9fe60407528b319977c5ce1e44d97ea75d6307d8
SimonLee9 <lbj142632@gmail.com>
2025-02-10 16:13:46 +0900

    start ui change by deploy

--- start_UI.sh
+++ start_UI.sh
@@ -3,4 +3,7 @@
 sleep 3
 #source ~/.bashrc
 
+export LD_LIBRARY_PATH=/home/rainbow/slamnav2/:$LD_LIBRARY_PATH
+
+
 gnome-terminal -- bash -c "cd /home/rainbow/robot_ui/ && ./RBM_S100_NewUI; exec bash"
